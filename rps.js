05d1ba0d326a3a53376a4bed7082e900fbbd5e07
Miyahara <25MiyaharaB@badinhs.org>
2025-03-03 13:21:20 -0500

    testing to see if we unlocked the problem

---
+++ rps.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let prompt(Rock") = 1;
+let prompt(Paper) = 2;
+let prompt(scissors) = 3;
+
+
+console.log(Rock)
+
+Math.random(1, 2, 3);
+console.log(Math.random);
